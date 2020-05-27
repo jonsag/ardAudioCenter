8595b4192550d902ea828b2aea509984654d7ddf
jonsag <jonsagebrand@gmail.com>
2020-05-27 19:05:17 +0200

    ...

--- ardAudioCenter/lcd.h
+++ ardAudioCenter/lcd.h
@@ -12,19 +12,23 @@ void printSource() {
 void printVolume() {
   lcd.setCursor(4, 1);
   lcd.print(volume);
-  lcd.setCursor(6, 1);
-  lcd.print("%");
   if (volume < 10) {
+    lcd.setCursor(5, 1);
+    lcd.print("%");
     for (int i = 6; i <= lcdColumns - 1; i++) {
       lcd.setCursor(i, 1);
       lcd.print(" ");
     }
   } else if (volume < 100) {
+    lcd.setCursor(6, 1);
+    lcd.print("%");
     for (int i = 7; i <= lcdColumns - 1; i++) {
       lcd.setCursor(i, 1);
       lcd.print(" ");
     }
   } else {
+    lcd.setCursor(7, 1);
+    lcd.print("%");
     for (int i = 8; i <= lcdColumns - 1; i++) {
       lcd.setCursor(i, 1);
       lcd.print(" ");
@@ -37,7 +41,7 @@ void printFunction() {
   lcd.print(functions[functionNo]);
   lcd.setCursor(3, 1);
   lcd.print(" ");
-  
+
   if (functions[functionNo] == "Vol") {
     printVolume();
   } else {
